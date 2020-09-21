c105ad0b4f156dfebfedf0f311b3e7d1b5964a82
Chase Moskal <chasemoskal@gmail.com>
2020-09-21 14:49:23 -0700

    implement: ui for writing desk

--- source/components/writing-desk/desk.css.ts
+++ source/components/writing-desk/desk.css.ts
@@ -2,8 +2,81 @@
 import {css} from "../../app/component.js"
 export default css`
 
-:host {
-	outline: 1px solid yellow;
+.container {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	margin: 1em auto;
+}
+
+.container > * {
+	flex: 1 0 auto;
+}
+
+.textinput, .resultbox {
+	min-width: 30em;
+	padding: 0.5em 1em;
+}
+
+.textinput {
+	font: inherit;
+	width: 100%;
+	min-height: 8em;
+	font-size: 1.2em;
+	color: white;
+	background: #0006;
+	border: 1px solid #fff6;
+}
+
+.resultbox {
+	flex: 0 0 auto;
+}
+
+.link {
+	color: white;
+	font-size: 1.5em;
+	text-shadow: 0 0 4px #fffb;
+}
+
+.link * {
+	display: inline-block;
+	vertical-align: middle;
+}
+
+.stats {
+	margin-top: 0.5em;
+	padding-left: 2.5em;
+}
+
+.stats > * + * {
+	margin-top: 0.5em;
+}
+
+.stats button.copybutton {
+	display: inline-block;
+	padding: 0.1em 0.5em;
+	min-width: 5em;
+}
+
+.copybutton > * {
+	display: inline-block;
+	vertical-align: middle;
+}
+
+.copybutton svg {
+	width: 1em;
+	height: 1em;
+}
+
+.copybutton[data-recently] {
+	position: relative;
+	opacity: 0.3;
+}
+
+@media (max-width: 700px) {
+	.textinput, .resultbox {
+		min-width: unset;
+	}
 }
 
 `
--- source/components/writing-desk/psafe-writing-desk.ts
+++ source/components/writing-desk/psafe-writing-desk.ts
@@ -1,5 +1,4 @@
 
-import * as loading from "metalshop/dist/metalfront/toolbox/loading.js"
 import {makeDebouncer} from "metalshop/dist/metalfront/toolbox/debouncer.js"
 import {mixinStyles} from "metalshop/dist/metalfront/framework/mixin-styles.js"
 
@@ -7,7 +6,13 @@ import {PsafeWritingDeskProps} from "../../types.js"
 import {encryptMessageLink, hintSize} from "../../app/links.js"
 import {Component, html, property} from "../../app/component.js"
 
+import lockIcon from "../../icons/lock.svg.js"
+import clippyIcon from "../../icons/clippy.svg.js"
+
 import styles from "./desk.css.js"
+
+const clipboardAnim = 3000
+const clipboardSupport = !!navigator.clipboard.writeText
 
  @mixinStyles(styles)
 export class PsafeWritingDesk extends Component {
@@ -42,32 +47,67 @@ export class PsafeWritingDesk extends Component {
 		}
 	})
 
+	 @property({type: Number})
+	private lastCopy: number = 0
+
+	get recentlyCopied() {
+		const since = Date.now() - this.lastCopy
+		return since < clipboardAnim
+	}
+
 	render() {
 		if (!this.props) return null
-		const {messageLink, messageLinkPreview} = this
+		const {messageLink, messageLinkPreview, recentlyCopied} = this
 
 		const handleTextChange = (event: InputEvent) => {
 			const target = <HTMLTextAreaElement>event.target
 			this.message = target.value
+			this.messageLink = undefined
 			this.debouncer.queue()
 		}
 
+		const handleClickCopy = async() => {
+			if (messageLink) {
+				await navigator.clipboard.writeText(messageLink)
+				this.lastCopy = Date.now()
+				setTimeout(() => this.requestUpdate(), clipboardAnim + 1)
+			}
+		}
+
 		return html`
-			<h2>writing desk - new style and layout coming soon</h2>
-			<textarea
-				@change=${handleTextChange}
-				@keyup=${handleTextChange}
-			></textarea>
-			${messageLink ? html`
-				<div class=results>
-					<p class=link>
-						<a href=${messageLink}>
-							${messageLinkPreview}
-						</a>
-					</p>
-					<p class=stat>length ${messageLink.length}</p>
+			<div class=leadup>
+				<h2>somebody asked you for secret information</h2>
+				<p>pastesafe will encrypt your response</p>
+			</div>
+			<div class=container>
+				<div class=inputbox>
+					<textarea
+						class=textinput
+						placeholder="enter your secret response"
+						@change=${handleTextChange}
+						@keyup=${handleTextChange}
+					></textarea>
 				</div>
-			` : null}
+				<div class=resultbox>
+					${messageLink ? html`
+						<p class=link>
+							${lockIcon}
+							<a title="encrpyted message link" href=${messageLink}>
+								${messageLinkPreview}
+							</a>
+						</p>
+						<div class=stats data-coolinputs>
+							<p>your message is encrypted in this big link</p>
+							${clipboardSupport ? html`
+								<p>now just <button class=copybutton title="copy link to clipboard" ?data-recently=${recentlyCopied} @click=${handleClickCopy}>${clippyIcon} ${recentlyCopied ? "copied" : "copy"}</button> and send back this link</p>
+							` : html`
+								<p>now copy this link and send it back</p>
+							`}
+							<p>whoever sent you the invite has the key to decrypt the link</p>
+						</div>
+					` : null}
+				</div>
+			</div>
 		`
 	}
 }
